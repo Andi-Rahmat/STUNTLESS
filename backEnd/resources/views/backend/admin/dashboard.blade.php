08702eb309b55e84f2487f8221f8121790954640
Padail <muhammadfadhail96@gmail.com>
2025-08-29 14:30:04 +0700

    deploy

--- backEnd/resources/views/backend/admin/dashboard.blade.php
+++ backEnd/resources/views/backend/admin/dashboard.blade.php
@@ -69,7 +69,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
-                <div class="card-body">
+                <div class="card-body table-responsive">
                     <h5 class="card-title">Data Kunjungan <span>hari ini</span></h5>
                     <table class="table datatable">
                         <thead>
