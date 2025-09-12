105c058f06e7e2d2e151030a5c8dd070bc6947ef
Padail <muhammadfadhail96@gmail.com>
2025-09-12 20:07:46 +0700

    deploy

--- backEnd/resources/views/frontend/pages/welcome.blade.php
+++ backEnd/resources/views/frontend/pages/welcome.blade.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Stuntless Navbar</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="icon" href="{{asset('assets/img/asset/Stuntless FIX.png')}}" type="image/x-icon">
+    <title>STUNTLESS</title>
     <!-- Bootstrap CSS -->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
