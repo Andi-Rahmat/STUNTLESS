f34258d533b56340ea23322c0952fe1e5cbff6a3
Padail <muhammadfadhail96@gmail.com>
2025-10-01 14:52:33 +0700

    deploy

--- backEnd/app/Helpers/helper.php
+++ backEnd/app/Helpers/helper.php
@@ -41,8 +41,8 @@ if (!function_exists('notif')) {
             $pengukuranSebelumnya = $pengukuran->skip(1)->first() ?? null;
             $tanggalPengukuran = Carbon::parse(($pengukuranTerbaru)->tglPengukuran);
             $tanggalSekarang = Carbon::now();
+            $selisihBulan = $tanggalPengukuran->diffInMonths($tanggalSekarang);
             if($pengukuranSebelumnya != null){
-                $selisihBulan = $tanggalPengukuran->diffInMonths($tanggalSekarang);
                 $selisihBulanWarning = Carbon::parse(($pengukuranSebelumnya)->tglPengukuran)->diffInMonths(Carbon::parse(($pengukuranTerbaru)->tglPengukuran));
                 if($pengukuranTerbaru->zScore->tinggi <= -2 && $pengukuranSebelumnya->zScore->tinggi <= -2 && $selisihBulanWarning >= 1 ){
                     $warning[$i] = $pengukuranTerbaru;
